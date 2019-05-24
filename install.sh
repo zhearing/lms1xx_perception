25c7444c46244cdfa4b408ba081bced1fdfa2453
zhearing <zyzhong@mail.ustc.edu.cn>
2019-05-24 10:22:31 +0800

    debug

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sudo apt-get -y install libgsl-dev
 sudo apt-get install -y cmake libeigen3-dev libopenblas-dev  liblapack-dev libarpack* libarmadillo*
-cd /catkin_ws/src/lms1xx_perception/libeigen-3.3.7 && mkdir build && cd build && cmake .. && make install
+cd /catkin_ws/src/lms1xx_perception/libeigen-3.3.7 && mkdir build && cd build && cmake .. && make install && cd /usr/local/include && sudo ln -sf eigen3/Eigen Eigen
